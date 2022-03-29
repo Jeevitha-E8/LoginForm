58fc8afca3ed52f82f1f6715dca51215e5f8c92d
Jeevitha-E8 <singurujeevitha@gmail.com>
2022-03-29 22:21:25 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'signinform';
+export class AppComponent implements OnInit {
+  myform:FormGroup
+  
+  ngOnInit()
+  { 
+    this.myform=new FormGroup({
+        'name':new FormControl(null,Validators.required),
+        'email':new FormControl(null,[Validators.required,Validators.email]),
+        'password':new FormControl(null,Validators.required),
+        'message':new FormControl(null,[Validators.required,Validators.minLength(5)])
+      });
+  }
+  
+  formmy(){
+    console.log(this.myform.value);
+    this.myform.reset()
+  }
 }
